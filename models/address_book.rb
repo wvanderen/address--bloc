eb86afff4e460277f5c6def47b90c5a4238ec4d1
William Van Deren <wvanderen@gmail.com>
2017-09-28 21:18:50 +0000

    checkpoint4-assignment

--- models/address_book.rb
+++ models/address_book.rb
@@ -17,4 +17,15 @@ require_relative 'entry'
         end
         entries.insert(index, Entry.new(name, phone_number, email))
     end
+    
+    def remove_entry(name, phone_number, email)
+        index = 0
+        entries.each do |entry|
+            if name = entry.name
+                break
+            end
+            index+=1
+        end 
+        entries.delete_at(index)
+    end
 end
--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -36,5 +36,15 @@ RSpec.describe AddressBook do
             expect(new_entry.email).to eq('augusta.king@lovelace.com')
         end
     end
+    
+    describe "#remove_entry" do
+        it "removes a single entry from the address book" do
+            book = AddressBook.new
+            book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+            book.remove_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+            
+            expect(book.entries.size).to eq(0)
+        end
+    end
 end
             
